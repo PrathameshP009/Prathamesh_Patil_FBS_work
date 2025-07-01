0f3dbf54088dbccfae24e3172b74e939decd6d77
PrathameshP009 <prathameshpatil12340@gmail.com>
2025-07-01 13:47:42 +0530

    Add files via upload

---
+++ test/ex3.py
@@ -0,0 +1,7 @@
+km = float(input("Enter the distance in kilometers :"))
+
+meters = km * 1000
+centimeters = km * 10000
+
+print(f"\n Distance in meters ; {meters} m")
+print(f"\n Distance in centimeters : {centimeters} cm")
